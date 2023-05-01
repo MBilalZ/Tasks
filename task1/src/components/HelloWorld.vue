0b8fc6592929f800f8b143467f1104c8f5b7e685
MBilalZ <bilalzahid530@gmail.com>
2023-05-01 21:05:09 +0500

    initial commit

---
+++ task1/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="getData">Get Data</button>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    getData() {
+      axios.get('https://api.publicapis.org/entries')
+        .then(response => {
+          console.log(response.data)
+          this.items = response.data.entries;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
